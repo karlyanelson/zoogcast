2f9c1375f9ae8b546c98f7a0d11be5a91f4ac649
Nelson, Karly <karly.nelson@hey.com>
2021-05-14 12:00:07 -0500

    setup tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
 // docs: https://www.11ty.io/docs/config/
+
+const now = String(Date.now());
+
 module.exports = function (eleventyConfig) {
+  // https://css-tricks.com/eleventy-starter-with-tailwind-css-alpine-js/
+  eleventyConfig.addWatchTarget("./_tmp/style.css");
+  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
+  eleventyConfig.addShortcode("version", function () {
+    return now;
+  });
+
   return {
     dir: {
       input: "src",
